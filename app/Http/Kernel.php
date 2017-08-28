d0618ef0108442659b172c497d8692393ff81fd8
Andrea Armiliato <armi@Andreas-MBP.station>
2017-08-28 17:37:48 +0200

    updates

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -18,6 +18,7 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+        \App\Http\Middleware\HttpsProtocol::class,
         \Barryvdh\Cors\HandleCors::class,
     ];
 
@@ -35,6 +36,7 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \App\Http\Middleware\HttpsProtocol::class,
             \Barryvdh\Cors\HandleCors::class,
         ],
 
