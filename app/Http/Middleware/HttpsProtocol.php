7e2b8543bb22ee497a2f942c832e0ce458938fd3
Andrea Armiliato <armi@Andreas-MBP.station>
2017-08-28 17:44:23 +0200

    updates

--- app/Http/Middleware/HttpsProtocol.php
+++ app/Http/Middleware/HttpsProtocol.php
@@ -6,12 +6,12 @@ use Closure;
 
 class HttpsProtocol {
 
-public function handle($request, Closure $next)
-{
-if (!$request->secure() && env('APP_ENV') === 'prod') {
-return redirect()->secure($request->getRequestUri());
-}
+    public function handle($request, Closure $next)
+    {
+        if (!$request->secure() && env('APP_ENV') === 'prod') {
+            return redirect()->secure($request->getRequestUri());
+        }
 
-return $next($request);
-}
+        return $next($request);
+    }
 }
